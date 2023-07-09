b6ce96006a721693b25b09c21c1d01dd29281833
Amirkaramat <amir_karamat@yahoo.com>
2023-07-09 13:51:52 +0330

    Update Header.jsx

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,15 +1,29 @@
-import { DownOutlined, SmileOutlined } from "@ant-design/icons";
-import { Dropdown, Space } from "antd";
+import { Menu } from "antd";
 import { useHomeState } from "../../../hooks";
+import { PlusOutlined, LineOutlined } from "@ant-design/icons";
 const Header = () => {
   const { menus } = useHomeState();
-  console.log(menus);
+  const items = menus.map((menu) => {
+    return {
+      key: menu.id,
+      className: "text-xl text-center",
+      label: menu.title,
+      icon: menu.subMenu ? <PlusOutlined /> : null,
+      children:
+        menu.subMenu &&
+        menu.subMenu.map((sub) => {
+          return {
+            key: sub.title,
+            label: sub.title,
+            className:"text-zinc-100 bg-zinc-800 text-lg",
+            icon: <LineOutlined />,
+          };
+        }),
+    };
+  });
   return (
-    <div className="flex">
-      {menus.map(
-        (menu) =>
-          menu.subMenu && <Dropdown menu={menu.subMenu}>{menu.title}</Dropdown>
-      )}
+    <div className='h-screen'>
+      <Menu className='text-xl p-2' mode='horizontal' items={items} />
     </div>
   );
 };
