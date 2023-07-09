5343cadb5a25a07dd2aaeb18b7dbc52f466ba20d
Amirkaramat <amir_karamat@yahoo.com>
2023-07-09 10:51:35 +0330

    Udate homeSlice.js

--- src/redux/slices/homeSlice/HomeSlice.js
+++ src/redux/slices/homeSlice/HomeSlice.js
@@ -1,14 +1,34 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
+
 const initialState = {
   loading: "idle",
   data: [],
   error: null,
 };
+import { BASE_URL } from "../../../api/index";
 
-const getHomeData = createAsyncThunk("home/getHomeData", async () => {
+export const getHomeData = createAsyncThunk("home/getHomeData", async () => {
   return await axios
-    .get()
+    .get(`${BASE_URL}/home_page`)
     .then((response) => response.data)
     .catch((err) => err.message);
 });
+
+const homeSlice = createSlice({
+  name: "home",
+  initialState,
+  extraReducers: (builder) => {
+    builder.addCase(getHomeData.pending, (state) => {
+      state.loading = "pending";
+    });
+    builder.addCase(getHomeData.fulfilled, (state, action) => {
+      (state.loading = "fulfilled"), (state.data = action.payload);
+    });
+    builder.addCase(getHomeData.rejected, (state, action) => {
+      (state.loading = "rejected"), (state.error = action.payload);
+    });
+  },
+});
+
+export default homeSlice.reducer
