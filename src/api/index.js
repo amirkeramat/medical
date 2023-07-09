ae122387409d75c000026c279e5941d5cdc8b741
Amirkaramat <amir_karamat@yahoo.com>
2023-07-09 10:53:27 +0330

    Update api

---
+++ src/api/index.js
@@ -0,0 +1,4 @@
+export const BASE_URL = "https://attractive-trench-coat-eel.cyclic.app";
+
+//For Get Home Page Data use /home_page with get method
+//"https://attractive-trench-coat-eel.cyclic.app/home_page"
