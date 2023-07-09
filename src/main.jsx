979280fee3e49e3cada178603c1824edb301e7c8
Amirkaramat <amir_karamat@yahoo.com>
2023-07-09 10:52:02 +0330

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import store from "./redux/store.js";
+import { Provider } from "react-redux";
 import "./index.css";
 import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <HashRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </HashRouter>
 );
