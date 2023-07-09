1cfc1035aa1c208f3a74b40975bfb2ffc2cb4dc6
Amirkaramat <amir_karamat@yahoo.com>
2023-07-09 10:52:12 +0330

    Udate Layout,jsx

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
-
-const Layout = () => {
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { getHomeData } from "../redux/slices/homeSlice/homeSlice";
+import Header from "../components/layout/header/Header";
+import Footer from "../components/layout/footer/Footer";
+import { useHomeState } from "../hooks";
+const Layout = ({ children }) => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getHomeData());
+  }, []);
+  const { loading } = useHomeState();
   return (
-    <div>Layout</div>
-  )
-}
+    <>
+      {loading === "fulfilled" ? (
+        <>
+          <Header />
+          <main>{children}</main>
+          <Footer />
+        </>
+      ) : (
+        <p> Loading</p>
+      )}
+    </>
+  );
+};
 
-export default Layout
+export default Layout;
