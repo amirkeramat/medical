d7d32864c22826c8c4d43639ddff090f1077f9c2
Amirkaramat <amir_karamat@yahoo.com>
2023-07-09 21:57:12 +0330

    Create About.jsx

---
+++ src/components/home/about/About.jsx
@@ -0,0 +1,31 @@
+import { useHomeState } from "../../../hooks";
+import { CheckOutlined, LeftCircleOutlined } from "@ant-design/icons";
+import {Button} from 'antd'
+const About = () => {
+  const { about } = useHomeState();
+  const { label, title, cover, description, items } = about;
+  return (
+    <section className="w-full grid grid-cols-1 md:grid-cols-2 p-4 shadow-sm shadow-zinc-950/10 container">
+      <img className="w-full h-full md:h-[500px]" src={cover} alt="" />
+      <div className="p-4">
+        <h6 className="text-teal-500 text-xl p-2">{label}</h6>
+        <h5 className="text-2xl font-semibold p-2">{title}</h5>
+        <p className="text-zinc-700 p-2">{description}</p>
+        <div className="p-2">
+          {items.map((item) => (
+            <p className="p-2 flex items-center" key={item.id}>
+              <CheckOutlined className="text-teal-500" />
+              {item.title}
+            </p>
+          ))}
+        </div>
+        <Button size="large" className="bg-teal-500 text-zinc-50 flex items-center mx-2 my-4">
+          بیشتر بدانید
+          <LeftCircleOutlined />
+        </Button>
+      </div>
+    </section>
+  );
+};
+
+export default About;
