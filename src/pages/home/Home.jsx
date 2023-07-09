37235cc291cfd926d019440bf233d93b417b8e64
Amirkaramat <amir_karamat@yahoo.com>
2023-07-09 21:55:50 +0330

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,17 @@
-
+import About from "../../components/home/about/About"
+import Expertise from "../../components/home/expertise/Expertise"
+import Intro from "../../components/home/intro/Intro"
+import Counter from "../../components/home/counter/Counter"
+import Services from "../../components/home/services/Services"
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="w-full flex flex-col items-center">
+      <Intro/>
+      <About/>
+      <Expertise/>
+      <Counter/>
+      <Services/>
+    </div>
   )
 }
 
