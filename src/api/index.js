aeb2e70f645ec8041dad859c87dd4159e0813a12
Amirkaramat <amir_karamat@yahoo.com>
2023-07-11 16:45:45 +0330

    Update Index.js

--- src/api/index.js
+++ src/api/index.js
@@ -2,3 +2,10 @@ export const BASE_URL = "https://attractive-trench-coat-eel.cyclic.app";
 
 //For Get Home Page Data use /home_page with get method
 //"https://attractive-trench-coat-eel.cyclic.app/home_page"
+
+
+// for get singleService use id of serviceItem
+//"https://attractive-trench-coat-eel.cyclic.app/services/[id]"
+
+// for get singleDoctor use id of DoctorsItem
+//"https://attractive-trench-coat-eel.cyclic.app/doctors/[id]"
