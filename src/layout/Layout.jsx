9d0e56501197999e767654888f71385c995382aa
Amirkaramat <amir_karamat@yahoo.com>
2023-07-11 21:52:12 +0330

    update layout.jsx

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,28 +1,31 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getHomeData } from "../redux/slices/HomeSlice";
 import Header from "../components/layout/header/Header";
 import Footer from "../components/layout/footer/Footer";
 import { useHomeState } from "../hooks";
 import PageLoader from "../components/shared/pageLoader/PageLoader";
+
 const Layout = ({ children }) => {
+  const {data} = useSelector((state) => state.homeState);
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(getHomeData());
+    if (!Object.keys(data).length) {
+      dispatch(getHomeData());
+    }
   }, []);
   const { loading } = useHomeState();
   return (
     <>
       {loading === "fulfilled" ? (
-        <div className="w-full flex flex-col items-center">
+        <div className='w-full flex flex-col items-center'>
           <Header />
-          <main className="w-full h-full pt-12">{children}</main>
+          <main className='w-full h-full pt-12'>{children}</main>
           <Footer />
         </div>
       ) : (
-        <PageLoader/>
+        <PageLoader />
       )}
-      
     </>
   );
 };
