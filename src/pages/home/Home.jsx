b0aebec1d4ef68beb96820ca7aa9845ec0710d65
Amirkaramat <amir_karamat@yahoo.com>
2023-07-11 09:15:33 +0330

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,11 @@ import Expertise from "../../components/home/expertise/Expertise"
 import Intro from "../../components/home/intro/Intro"
 import Counter from "../../components/home/counter/Counter"
 import Services from "../../components/home/services/Services"
+import Doctors from "../../components/home/doctors/Doctors"
+import Appointments from "../../components/home/appointments/Appointments"
+import Comments from "../../components/home/comments/Comments"
+import Question from "../../components/home/question/Question"
+import Articles from "../../components/home/articles/Articles"
 const Home = () => {
   return (
     <div className="w-full flex flex-col items-center">
@@ -11,6 +16,11 @@ const Home = () => {
       <Expertise/>
       <Counter/>
       <Services/>
+      <Doctors/>
+      <Appointments/>
+      <Comments/>
+      <Articles/>
+      <Question/>
     </div>
   )
 }
