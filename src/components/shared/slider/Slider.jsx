cdbe4957ff7d44e90afbdf7237a2af4249f034fc
Amirkaramat <amir_karamat@yahoo.com>
2023-07-11 16:44:02 +0330

    Create Slider.jsx

---
+++ src/components/shared/slider/Slider.jsx
@@ -0,0 +1,12 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+
+import "swiper/css/navigation";
+
+import { Autoplay, Navigation } from "swiper/modules";
+const Slider = () => {
+  return <div>Slider</div>;
+};
+
+export default Slider;
