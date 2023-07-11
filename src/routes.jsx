36b9a33aef770d90a60ea5665ef9ecc70094cbb9
Amirkaramat <amir_karamat@yahoo.com>
2023-07-11 16:48:01 +0330

    fix routes.jsx

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,7 +2,7 @@ import Home from "./pages/home/Home";
 import Service from "./pages/services/service/Service";
 import Services from "./pages/services/Services";
 import Doctors from "./pages/doctors/Doctors";
-import Doctor from "./pages/doctors/doctor/doctor";
+import Doctor from "./pages/doctors/doctor/Doctor";
 const routes = [
   { path: "/", element: <Home /> },
   { path: "services", element: <Services /> },
