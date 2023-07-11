20ab857a9fd523694111ecac5fc4fff8538db709
Amirkaramat <amir_karamat@yahoo.com>
2023-07-11 09:39:56 +0330

    fix main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import store from "./redux/store.js";
+import store from "./redux/store";
 import { Provider } from "react-redux";
 import "./index.css";
 import { HashRouter } from "react-router-dom";
